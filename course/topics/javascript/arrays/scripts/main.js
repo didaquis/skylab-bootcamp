d3a0d6a6e35486f4b55ebefc89904a9cb63834bb
Dídac García <didaquis@gmail.com>
2018-01-17 09:43:26 +0100

    :memo: Añado un ejemplo de recursividad

--- course/topics/javascript/arrays/scripts/main.js
+++ course/topics/javascript/arrays/scripts/main.js
@@ -13,3 +13,37 @@ console.log(isArray('Skylab'));    // this should returns false
 console.log(isArray([3, 5]));      // this should returns true
 
 
+
+// ### joinElements
+function joinElements(elements){
+	return elements.join(',');
+}
+
+var beatles = ['John','George','Ringo','Paul'];
+console.log(joinElements(beatles));
+
+
+
+
+// ### array_Clone
+function array_Clone(source){
+	var array_result = [];
+
+	for (var i = 0; i < source.length; i++) {
+		if( isArray(source[i]) ){
+			// Ejemplo de recursividad
+			array_result.push( array_Clone(source[i]) );
+		}else{
+			array_result.push(source[i]);
+		}
+	}
+	return array_result;
+}
+
+//console.log(array_Clone([1, 2, 4, 0])); // [1, 2, 4, 0]
+console.log(array_Clone([1, 2, [4, 0]])); // [1, 2, [4, 0]]
+
+
+
+
+
