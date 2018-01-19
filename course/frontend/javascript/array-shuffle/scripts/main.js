26cdec7a66cab0ad3b0a97621305d6d17510462f
Dídac García <didaquis@gmail.com>
2018-01-19 13:53:21 +0100

    :recycle: Renombro el método y alguna variable

--- course/frontend/javascript/array-shuffle/scripts/main.js
+++ course/frontend/javascript/array-shuffle/scripts/main.js
@@ -6,37 +6,36 @@ Array.prototype.showMeTheArray = function (){
 // # Challenge ShuffleArray!
 
 /**
- * Array.random() Crea un clon exacto de un array y desordena aleatoriamente sus elementos.
+ * Array.shuffle() Crea un clon exacto de un array y desordena aleatoriamente sus elementos.
  *
  * @code
  *  var originalArray = [1,2,3,'a','b','c'];
- * 	var newArray = originalArray.random();
+ * 	var newArray = originalArray.shuffle();
  * @endcode
  *
- * @return {array} clonedArray. Retorna un nuevo array.
+ * @return {array} newArray. Retorna un nuevo array.
  */
-Array.prototype.random = function (){
-	var clonedArray = [];
+Array.prototype.shuffle = function (){
+	var newArray = [];
 	for (var i = 0; i < this.length; i++) {
-		clonedArray.push(this[i]);
+		newArray.push(this[i]);
 	}
-	// clonedArray == just a clone of "this" (the original array)!
-	var currentIndex = clonedArray.length;
-	var temporaryValue, randomIndex;
+	// newArray == just a clone of "this" (the original array)!
+	var currentIndex = newArray.length;
+	var tempValue, randomIndex;
 	while (0 !== currentIndex) {
 		randomIndex = Math.floor(Math.random() * currentIndex); // cojo un elemento al azar
 		currentIndex -= 1;
-		temporaryValue = clonedArray[currentIndex]; // Me guardo el valor de la posición "A"
-		clonedArray[currentIndex] = clonedArray[randomIndex]; // Copio el valor de "B" en la posición "A"
-		clonedArray[randomIndex] = temporaryValue; // Copio el valor de "A" en la posición "B"
+		tempValue = newArray[currentIndex]; // Me guardo el valor de la posición "A"
+		newArray[currentIndex] = newArray[randomIndex]; // Copio el valor de "B" en la posición "A"
+		newArray[randomIndex] = tempValue; // Copio el valor de "A" en la posición "B"
 	}
-	return clonedArray;
+	return newArray;
 }
 
 var a = [1,2,3,'a','b','c'];
 console.table(a);
-var b = a.random(); // Llamo al nuevo método
+var b = a.shuffle(); // Llamo al nuevo método
 console.table(b);
 console.log( (a === b)? "Los arrays son iguales": " Los arrays son distintos");
-
 
