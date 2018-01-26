a5209a4ae6a193cb188bff033c7f531f90a54fc1
Dídac García <didaquis@gmail.com>
2018-01-26 14:54:09 +0100

    :recycle: Mejorado aún más el ejercicio

--- course/frontend/todo-list-jquery/scripts/main.js
+++ course/frontend/todo-list-jquery/scripts/main.js
@@ -1,16 +1,17 @@
 $( "form" ).submit(function( event ) {
 	event.preventDefault();
 
-	var textOfTask = $('#task').val();
+	var textOfTask = $('input[name="task"]').val(); // observa que el selector está devolviendo un array. Sin embargo, al contener solo una posición me lo interpreta como un solo elemento y no como un conjunto de elementos.
 
-	$('#task').focus();
-	$('#task').val('');
+	$('input[name="task"]').focus();
+	$('input[name="task"]').val('');
 
-	$("ul").append('<li>' + textOfTask + ' | <a> 🗑️ </a>' + '</li>');
+	$("ul").append('<li>' + textOfTask + '<span>|</span><a>🗑️ </a>' + '</li>');
 });
 
-$("body").click( function(event){
-	if(event.target.tagName.toLowerCase() === 'a'){
-		$(event.target).parent().remove();
+$("body").click(function(event){
+	var target = $(event.target);
+	if( target.is("a") ){
+		target.parent().remove();
 	}
-} );
+});
