324304252a2a045cb20756a4f9944f1717534a08
Dídac García <didaquis@gmail.com>
2018-01-26 13:42:36 +0100

    :recycle: Convert spaces to tabs

--- course/frontend/todo-list-jquery/scripts/main.js
+++ course/frontend/todo-list-jquery/scripts/main.js
@@ -3,15 +3,15 @@ $( "form" ).submit(function( event ) {
 
 	var textOfTask = $('#task').val();
 
-  	$('#task').focus();
-  	$('#task').val('');
+	$('#task').focus();
+	$('#task').val('');
 
 	$("ul").append('<li>' + textOfTask + ' | <a> 🗑️ </a>' + '</li>');
 
 });
 
 $("body").click( function(event){
-    if(event.target.tagName.toLowerCase() === 'a'){
-    	event.target.parentNode.remove();
-    }
+	if(event.target.tagName.toLowerCase() === 'a'){
+		event.target.parentNode.remove();
+	}
 } );
