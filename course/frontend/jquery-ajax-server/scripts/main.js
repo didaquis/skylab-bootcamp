2547f11795f8ea53e686dca4f0aea3c790b1a692
Dídac García <didaquis@gmail.com>
2018-01-27 10:12:49 +0100

    :memo: Incorporo un ejemplo de filtrado de datos

--- course/frontend/jquery-ajax-server/scripts/main.js
+++ course/frontend/jquery-ajax-server/scripts/main.js
@@ -32,3 +32,42 @@ $(document).ready(function(){
 	}
 
 });
+
+
+
+
+/**
+ * Considera este ejemplo.
+ * 
+ * Si en el index hubiera un formulario con un input, podríamos buscar entre los datos.
+ *
+ */
+
+/*
+$('form').submit(function (e) {
+    e.preventDefault();
+
+    var query = $('input').val();
+
+    $.ajax({
+        url: "data/staff.json",
+        success: function (result) {
+            console.log(result);
+
+            var list = '';
+
+            var filtered = result.filter(function(v) {
+                return v.name.indexOf(query) !== -1;
+            })
+        
+            filtered.forEach(function(v) {
+                list += v.name + ', '; 
+            });
+
+            $("#box").html(list);
+        }
+    });
+});
+*/
+
+
