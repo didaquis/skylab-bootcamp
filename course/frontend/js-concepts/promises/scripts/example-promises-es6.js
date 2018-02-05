08c660248377e1bc7dfb2683c078dc30e3feb1d2
Dídac García <didaquis@gmail.com>
2018-02-05 14:40:21 +0100

    :pencil: Nuevo ejemplo de promises

---
+++ course/frontend/js-concepts/promises/scripts/example-promises-es6.js
@@ -0,0 +1,35 @@
+/* Chained promises (JS ES6 syntax) */
+const isMomHappy = true;
+
+// Promise
+const willIGetNewPhone = new Promise(
+	(resolve, reject) => {
+		if (isMomHappy) {
+			const phone = {
+				brand: 'Samsung',
+				color: 'black'
+			};
+			resolve(phone);
+		} else {
+			const reason = new Error('mom is not happy');
+			reject(reason);
+		}
+
+	}
+);
+
+const showOff = function (phone) {
+	const message = 'Hey friend, I have a new ' +
+				phone.color + ' ' + phone.brand + ' phone';
+	return Promise.resolve(message);
+};
+
+// call our promise
+const askMom = function () {
+	willIGetNewPhone
+		.then(showOff)
+		.then(fulfilled => console.log(fulfilled))
+		.catch(error => console.log(error.message));
+};
+
+askMom();
