d44dbe5e1d1171414561440419b015b2a6192c33
Dídac García <didaquis@gmail.com>
2018-02-19 17:55:57 +0100

    :recycle: Mejorado el ejemplo de React Router Dom

--- course/frontend/react/react-router-basic-example/src/components/Nav.js
+++ course/frontend/react/react-router-basic-example/src/components/Nav.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Nav(props) {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 	  		<div className="container">
-				<Link to='/' className="navbar-brand">Start Bootstrap</Link>
+				<NavLink to='/' className="navbar-brand">Start Bootstrap</NavLink>
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 		  			<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarResponsive">
 		  			<ul className="navbar-nav ml-auto">
 						<li className="nav-item">
-			  				<Link to='/' className="nav-link">Home</Link>
+			  				<NavLink exact to='/' activeClassName='active' className="nav-link">Home</NavLink>
 						</li>
 						<li className="nav-item">
-			  				<Link to='/about' className="nav-link">About</Link>
+			  				<NavLink to='/about' activeClassName='active' className="nav-link">About</NavLink>
 						</li>
 						<li className="nav-item">
-			  				<Link to='/services' className="nav-link">Services</Link>
+			  				<NavLink to='/services' activeClassName='active' className="nav-link">Services</NavLink>
 						</li>
 		  			</ul>
 				</div>
@@ -31,9 +31,11 @@ function Nav(props) {
 export default Nav;
 
 /**
- * Importo 'Link'
- * Sustituyo todos las etiquetas html 'a' por componentes 'Link'
- * Observa que cada uno de estos componentes 'Link' tiene un atributo llamado 'to' 
+ * Importo 'NavLink'
+ * Sustituyo todos las etiquetas html 'a' por componentes 'NavLink'
+ * Observa que cada uno de estos componentes 'NavLink' tiene un atributo llamado 'to' 
  * En esos atributos 'to', pondré el valor de la URL a la que quiero que apunte cada uno de ellos
  * Esos componentes no deben de llevar el atributo 'href'
+ * Fíjate que puedo especificar una clase css que ese elemento heredará si la url coincide con la del elemento
+ * Es clase la defines con el parámetro 'activeClassName'
  */
