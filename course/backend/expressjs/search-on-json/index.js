d71b1b610cde6ca001675882c43c5e587fc05e66
Dídac García <didaquis@gmail.com>
2018-02-22 15:47:07 +0100

    :recycle: Mejorado el ejercicio

--- course/backend/expressjs/search-on-json/index.js
+++ course/backend/expressjs/search-on-json/index.js
@@ -1,31 +1,41 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
-const jsonBodyParser = bodyParser.json();
 
 const staffData = require('./staff.json');
+/**
+ * La linea anterior también podría ser así:
+ * 
+ * const fs = require('fs');
+ * const staffData = JSON.parse(fs.readFileSync('./staff.json', 'utf-8'));
+ */
 
 
-app.get('/api/staff', jsonBodyParser, (req, res) => {
+app.get('/api/staff', (req, res) => {
 
-	let filteredStuffData = staffData.filter((staff) => {
-		if(staff.name === req.query.q){
-			return staff
-		}
-	})
+	const query = req.query.q || req.query.query;
 
-	res.send(filteredStuffData);
+	if(query){
+		// Si me llega una clave GET válida...
+
+		let results = staffData.filter( (person) => person.name.includes(query) || person.surname.includes(query) || person.email.includes(query) )
+
+		res.json(results); /* Fíjate que las respuestas se las envío en json */
+	}else{
+		// Si no me llega una clave válido o es igual a '' (string vacío), le envío la totalidad de resultados
+		res.json(staffData)
+	}
 });
 
-const port = process.argv[2]
+
+require('dotenv').config(); /* Cargo las variables de entorno de mi fichero '.env' */
+const port = process.env.PORT; /* Obtenemos el puerto de una variable de entorno */
+
 app.listen(port, () => console.log(`Server running on port: ${port}`));
 
 /* 
-Abrir el server así: node index.js 4567
-Hacer una petición por get así: localhost:7898/api/staff?q=Dídac
+Abrir el server así: node index.js
+Hacer una petición de tipo 'get' así: localhost:4501/api/staff?q=di
 
 Te retornará este JSON:
 
