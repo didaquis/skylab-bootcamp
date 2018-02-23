7f749434fdcabf3503cd9d7686b953f8628cd076
Dídac García <didaquis@gmail.com>
2018-02-24 00:11:46 +0100

    :pencil: Primeros pasos creando apis con Express

--- course/backend/expressjs/users-api-json/index.js
+++ course/backend/expressjs/users-api-json/index.js
@@ -17,34 +17,6 @@ class User{
 	}
 }
 
-/* Si vamos a recibir info por 'json' usaremos 'bodyParser.json()' pero si la info llega por formulario trabajaríamos con 'bodyParser.urlencoded({extended:false})' */
-// En este caso, trabajamos con JSON.
-const jsonBodyParser = bodyParser.json();
-app.use(jsonBodyParser); /* Gracias a esta instrucción, todas las llamadas a 'app' usarán a partir de ahora 'jsonBodyParser'. */
-
-app.post('/api/users', (req, res) => {
-	// Por POST creamos nuevos usuarios
-	const usernameProvided = req.body.username;
-	const passwordProvided = req.body.password;
-
-	if(usernameProvided && passwordProvided){
-		// Si me llegan valores por POST...
-		
-		// Chequeo si ya existe un usuario con el mismo username
-		const usernameAlreadyExists = usersData.some( user => user.username === usernameProvided);
-		if (usernameAlreadyExists) {
-			return res.json( failed('User registration failed.', 'Username already exists.') ); // El return provoca que de deje de ejecutar el resto del código
-		}
-
-		// Dado que ha pasado las validaciones, creo un nuevo usuario y lo guardo:
-		let userRegistration = new User(usernameProvided, passwordProvided);
-		usersData.push(userRegistration);
-
-		res.json( success('User registration succeeded.') );
-	}else{
-		res.json( failed('User registration failed.', 'Invalid username and/or password.') );
-	}
-});
 
 app.get('/api/users', (req, res) => {
 	// Por GET mostramos todos los usuarios
@@ -56,15 +28,76 @@ app.get('/api/users', (req, res) => {
 });
 
 
-app.delete('/api/users', (req, res) => {
-	// Usamos DELETE para borrar usuarios
-	res.json('delete');
+app.get('/api/users/:username', (req, res) => {
+	// Por GET buscamos un usuario en concreto
+	const { params: { username:usernameProvided } } = req; /* Destructuring nivel ninja */
+
+	let user;
+	for (let i = 0; i < usersData.length; i++) {
+        const _user = usersData[i]
+
+        if (_user.username === usernameProvided) {
+            user = _user;
+            break;
+        }
+    }
+
+    if (user) {
+        res.json(success('User retrieval succeeded.', { username: user.username }))
+    } else {
+        res.json(failed('User retrieval failed.', 'Username does not exist.'))
+    }
+
 });
 
 
-app.put('/api/users', (req, res) => {
+/* Si vamos a recibir info por 'json' usaremos 'bodyParser.json()' pero si la info llega por formulario trabajaríamos con 'bodyParser.urlencoded({extended:false})' */
+// En este caso, trabajamos con JSON.
+const jsonBodyParser = bodyParser.json();
+app.use(jsonBodyParser); /* Gracias a esta instrucción, todas las llamadas a 'app' usarán a partir de ahora 'jsonBodyParser'. */
+
+app.post('/api/users', (req, res) => {
+	// Por POST creamos nuevos usuarios
+	const { username, password } = req.body;
+
+    if (!username || !password) return res.json(failed('User registration failed.', 'Invalid username and/or password.'));
+
+    const alreadyExists = usersData.some(user => user.username === username);
+
+    if (alreadyExists) return res.json(failed('User registration failed.', 'Username already exists.'));
+
+    usersData.push({ username, password });
+
+    res.json(success('User registration succeeded.'));
+});
+
+
+
+app.delete('/api/users/:username', (req, res) => {
+	// Usamos DELETE para borrar usuarios
+	const { params: { username } } = req;
+
+    const { password } = req.body;
+
+    if (!username || !password) return res.json(failed('User deletion failed.', 'Invalid username and/or password.'));
+
+    /* Interesante ejemplo de uso de 'findIndex' */
+    const index = usersData.findIndex(user => user.username === username);
+    const user = usersData[index];
+
+    if (!user) return res.json(failed('User deletion failed.', 'Username does not exists.'));
+
+    if (user.password === password) {
+        usersData.splice(index, 1);
+        res.json(success('User deletion succeeded'));
+    } else 
+        res.json(failed('User deletion failed.', 'Wrong username and/or password.'));
+});
+
+
+app.put('/api/users/:username', (req, res) => {
 	// Usamos UPDATE para actualizar el password de los usuarios
-	const usernameProvided = req.body.username;
+	const usernameProvided = req.params.username;
 	const passwordProvided = req.body.password;
 	const newpasswordProvided = req.body.newpassword;
 
@@ -84,8 +117,7 @@ app.put('/api/users', (req, res) => {
 		}
 	}else{
 		// no me llegan los datos necesarios
-		// 
-		// TO-DO
+		return res.json(failed('User update failed.', 'Invalid username and/or password.'));
 	}
 });
 
@@ -115,16 +147,19 @@ const port = process.env.PORT;
 app.listen(port, () => console.log(`landing server running on port: ${port}`));
 
 /*
+Para debugg, en el terminal (incorpora un breakpoint temporal al inicio): devtool index.js --watch --break
+
 
 Para reproducir el ejemplo:
 En un terminal ejecuta: node index.js 
 
 Usa Postman para crear usuarios: Envía peticiones de tipo POST enviando como 'body' tipo 'raw' y con formato 'json' lo siguiente:
 {
-	"username": "paco",
+	"username": "pepe",
 	"password": "1234"
 }
 
-También puedes usar Postman para listar los usuarios que hay almacenados (en memoria, ya que aún no tenemos base de datos). Para listarlos, haz una petición de tipo GET a localhost:4501/api/users
+Verás que puedes listar los usuarios, listar un único usuario, modificar el password y borrar el usuario.
+Encontrarás un 'json' con algunas llamadas de ejemplo con Postman en el directorio 'test-with-postman'.
 */
 
