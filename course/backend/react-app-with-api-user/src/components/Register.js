09157a6cb92d91dbc38c72227e2167039b957bad
Dídac García <didaquis@gmail.com>
2018-03-07 09:29:36 +0100

    :pencil: Ya se controlan los campos del form de registro usuario

--- course/backend/react-app-with-api-user/src/components/Register.js
+++ course/backend/react-app-with-api-user/src/components/Register.js
@@ -8,11 +8,11 @@ class Register extends Component {
 	constructor(){
 		super();
 
-		state: {
-			inputName: "";
-			inputSurname: "";
-			inputEmail: "";
-			inputUsername:"";
+		this.state = {
+			inputName: "",
+			inputSurname: "",
+			inputEmail: "",
+			inputUsername:"",
 			inputPassword: ""
 		};
 	}
@@ -62,23 +62,23 @@ class Register extends Component {
 								<h1 className="h3 mb-3 font-weight-normal">Register new user</h1>
 
 								<label htmlFor="inputName" className="sr-only">Name</label>
-								<input type="text" id="inputName" className="form-control mb-4" placeholder="Name of user" required autoFocus onChange={e => this.fillInput(e.target)}/>
+								<input type="text" id="inputName" className="form-control mb-4" placeholder="Name of user" required autoFocus onChange={e => this.fillInput(e.target)} value={this.state.inputName} />
 
 								<label htmlFor="inputSurname" className="sr-only">Surname</label>
-								<input type="text" id="inputSurname" className="form-control mb-4" placeholder="Surname of user" required onChange={e => this.fillInput(e.target)} />
+								<input type="text" id="inputSurname" className="form-control mb-4" placeholder="Surname of user" required onChange={e => this.fillInput(e.target)} value={this.state.inputSurname} />
 
 								<label htmlFor="inputEmail" className="sr-only">Email address</label>
-								<input type="email" id="inputEmail" className="form-control mb-4" placeholder="Email address" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}" required onChange={e => this.fillInput(e.target)} />
+								<input type="email" id="inputEmail" className="form-control mb-4" placeholder="Email address" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}" required onChange={e => this.fillInput(e.target)} value={this.state.inputEmail} />
 
 								<div className="form-group">
 									<label htmlFor="inputUsername" className="sr-only">Username</label>
-									<input type="text" id="inputUsername" className="form-control" placeholder="Username" required pattern="^[a-zA-Z0-9]{6,18}$" onChange={e => this.fillInput(e.target)} />
+									<input type="text" id="inputUsername" className="form-control" placeholder="Username" required pattern="^[a-zA-Z0-9]{6,18}$" onChange={e => this.fillInput(e.target)} value={this.state.inputUsername} />
 									<small id="emailHelp" className="form-text text-muted">Min 6 chars. Max 18 chars. You can use minus chars, mayus chars and numbers.</small>
 								</div>
 
 								<div className="form-group">
 									<label htmlFor="inputPassword" className="sr-only">Password</label>
-									<input type="password" id="inputPassword" className="form-control" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required onChange={e => this.fillInput(e.target)} />
+									<input type="password" id="inputPassword" className="form-control" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required onChange={e => this.fillInput(e.target)} value={this.state.inputPassword} />
 									<small id="emailHelp" className="form-text text-muted">At least of 8 chars. Must include minus chars, mayus chars and numbers.</small>
 								</div>
 								<button className="btn btn-lg btn-primary btn-block" type="submit">Create new user!</button>
